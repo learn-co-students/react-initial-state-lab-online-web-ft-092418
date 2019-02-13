52ff42462123d52950df1973d60a0c336ae47f6a
Peter-G-Stone <peter.stone.online@gmail.com>
2019-02-13 10:40:31 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,44 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            secondsLeft: this.props.initialCount
+        }
+    }
+
+    // componentDidMount() {
+    //     this.timer = setInterval(
+    //         () => this.tick(),
+    //         1000
+    //     )
+    // }
+
+    // componentWillUnmount() {
+    //     clearInterval(this.timer)
+    // }
+
+
+    writeText() {
+        if (this.state.secondsLeft === 0){
+            return 'Boom!'
+        } else {
+            return `${this.state.secondsLeft} seconds left before I go boom!`
+        }
+    }
+
+    // tick() {
+    //     this.setState((state, props) => ({
+    //         secondsLeft: state.secondsLeft - 1
+    //     }))
+    // }
+
+    render () {
+        return (
+            <div>{this.writeText()}</div>
+        )
+    }
+}
